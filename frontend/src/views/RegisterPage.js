33bb1985d3925e8c0cc586783623e53d5af81aeb
Juan Carlos Recio Abad <juan.carlos.recio.abad@gmail.com>
2025-01-12 15:27:03 +0100

    Introduce some temporary frontend

---
+++ frontend/src/views/RegisterPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PreregisterNavbar from "components/Navbars/PreregisterNavbar";
+import GoogleButton from "react-google-button";
+
+import {
+  Badge,
+  Button,
+  Card,
+  Form,
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+
+function User() {
+  const mainPanel = React.useRef(null);
+
+  return (
+    <>
+      <div className="wrapper">
+        {/* <Sidebar color={color} image={hasImage ? image : ""} routes={routes} /> */}
+        <div className="main-panel" ref={mainPanel}>
+          <PreregisterNavbar />
+          <div className="content">
+            <Container fluid>
+              <Row>
+                <Col md="12">
+                  <Card>
+                    <Card.Header>
+                      <Card.Title as="h4">Registrate</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                      <GoogleButton
+                        label="Login con Google"
+                        onClick={() => {
+                          console.log("Google button clicked");
+                        }}
+                      />
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default User;
